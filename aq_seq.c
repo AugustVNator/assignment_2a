30506984379d1508104d5c4b23ee2ac8c8f31b38
Raaschou <168561326+Raaschou@users.noreply.github.com>
2025-10-01 13:09:31 +0200

    Add first bit of aq seq

--- aq_seq.c
+++ aq_seq.c
@@ -5,17 +5,39 @@
  * @brief  Alarm queue skeleton implementation
  */
 
+#include <stdlib.h>
+
 #include "aq.h"
 
+typedef struct {
+  void *msg;
+  MsgKind K;
+  struct queueNode *next;
+} queueNode;
+
 AlarmQueue aq_create( ) {
+
+  queueNode *head = malloc(sizeof(queueNode));
+  AlarmQueue aq = &head;
+
+  if (aq != NULL) {
+    return aq;
+  }
   return NULL;
 }
 
 int aq_send( AlarmQueue aq, void * msg, MsgKind k){
+  if (k == AQ_ALARM /*&& aq->msg == AQ_ALARM*/) {
+    return AQ_NO_ROOM;
+  }
+
   return AQ_NOT_IMPL;
 }
 
 int aq_recv( AlarmQueue aq, void * * msg) {
+  if (msg == NULL) {
+    return AQ_NO_MSG;
+  }
   return AQ_NOT_IMPL;
 }
 
