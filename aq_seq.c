eb9ab84156dfd811aafad3fc1398288580042f04
Raaschou <168561326+Raaschou@users.noreply.github.com>
2025-10-01 22:40:24 +0200

    Add most of aq_recv function

--- aq_seq.c
+++ aq_seq.c
@@ -37,6 +37,22 @@ void insertAtEnd(queueNode **head, void *msg, MsgKind k) {
   temp->next = newQueueNode;
 }
 
+int deleteNode(queueNode **head, void **msg) {
+  queueNode **tempP = head;
+  queueNode *temp = *head;
+  while (temp != NULL) {
+    if (temp->msg == msg) {
+      int retval = temp->msgKind;
+      *tempP = temp->next;
+      free (temp);
+      return retval;
+    }
+    tempP = &temp->next;
+    temp = temp->next;
+  }
+  return -1;
+}
+
 AlarmQueue aq_create( ) {
   AlarmQueue **aq = malloc(sizeof(queueNode *));
 
@@ -46,7 +62,7 @@ AlarmQueue aq_create( ) {
     /**
      * Header specifies return value
      * should be "handle to alarm queue".
-     * It seems to be working 
+     * It seems to be working
      */
     return aq;
   }
@@ -82,12 +98,40 @@ int aq_send( AlarmQueue aq, void * msg, MsgKind k){
 
 int aq_recv( AlarmQueue aq, void * * msg) {
   if (msg == NULL) {
+    return AQ_NULL_MSG;
+  }
+
+  queueNode **head = aq;
+  queueNode *temp = *head;
+
+  // Check if queue might be empty
+  if (*head == NULL) {
     return AQ_NO_MSG;
   }
 
+  // Check if any of the messages are of alarm kind
+  while (temp != NULL) {
+    printf("I reach alarm\n");
+    if (temp->msgKind == AQ_ALARM && temp->msg == msg) {
+      printf("I reach inner alarm\n");
+      return deleteNode(head, msg);
+    }
+    temp = temp->next;
+  }
 
+  printf("I reach reset\n");
+  // Reset temp to head
+  temp = *head;
 
-  return AQ_NOT_IMPL;
+  while (temp != NULL) {
+    printf("I reach normal\n");
+    if (temp->msg == msg) {
+      printf("I reach inner normal\n");
+      return deleteNode(head, msg);
+    }
+    temp = temp->next;
+  }
+  return 0;
 }
 
 int aq_size( AlarmQueue aq) {
