bdecf4b1613a2bc0fafe0ac459d9960c2807383f
s245299 <s245299@dtu.dk>
2025-10-08 21:27:18 +0200

    Lots of way to go - understanding recv, send, mutex and cond better tho

--- aq_tsafe.c
+++ aq_tsafe.c
@@ -6,17 +6,131 @@
  */
 
 #include "aq.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+static pthread_cond_t  notEmpty  = PTHREAD_COND_INITIALIZER;
+static pthread_cond_t  alarmFree = PTHREAD_COND_INITIALIZER;
+
+typedef struct queueNode{
+  void *msg;
+  MsgKind msgKind;
+  struct queueNode *next;
+} queueNode;
+
+queueNode *createQueueNode(void *msg, MsgKind k) {
+  queueNode *newQueueNode = malloc(sizeof(queueNode));
+  newQueueNode->msg = msg;
+  newQueueNode->msgKind = k;
+  newQueueNode->next = NULL;
+  return newQueueNode;
+}
+
+void insertAtEnd(queueNode **head, void *msg, MsgKind k) {
+  queueNode *newQueueNode = createQueueNode(msg, k);
+  if (*head == NULL) {
+    *head = newQueueNode;
+    return;
+  }
+  queueNode *temp = *head;
+  while (temp->next != NULL) {
+    temp = temp->next;
+  }
+  temp->next = newQueueNode;
+}
+
+int deleteNode(queueNode **head, int msgContent, void **msg) {
+  queueNode **tempP = head;
+  queueNode *temp = *head;
+  while (temp != NULL) {
+    if (*(int*)temp->msg == msgContent) {
+      int retval = *(int*)temp->msg;
+      *msg = temp->msg;
+      *tempP = temp->next;
+      free (temp);
+      return retval;
+    }
+    tempP = &temp->next;
+    temp = temp->next;
+  }
+  return -1;
+}
 
 AlarmQueue aq_create( ) {
+  AlarmQueue **aq = malloc(sizeof(queueNode *));
+
+  if (aq != NULL) {
+    *aq = NULL;
+    return aq;
+  }
   return NULL;
 }
 
 int aq_send( AlarmQueue aq, void * msg, MsgKind k){
-  return AQ_NOT_IMPL;
+  if ( aq == NULL ) {
+    return AQ_UNINIT;
+  }
+  if (msg == NULL) {
+    return AQ_NULL_MSG;
+  }
+
+  pthread_mutex_lock(&lock);
+  queueNode **head = aq;
+  queueNode *temp = *head;
+
+  while (temp != NULL) {
+    if (temp->msgKind == AQ_ALARM && k == AQ_ALARM) {
+      pthread_mutex_unlock(&lock);
+      return AQ_NO_ROOM;
+    }
+    temp = temp->next;
+  }
+
+  insertAtEnd(head, msg, k);
+  pthread_mutex_unlock(&lock);
+  return 0;
 }
 
-int aq_recv( AlarmQueue aq, void * * msg) {
-  return AQ_NOT_IMPL;
+int aq_recv(AlarmQueue aq, void * * msg) {
+  if (msg == NULL) {
+    return AQ_NULL_MSG;
+  }
+  pthread_mutex_lock(&lock);
+  queueNode **head = aq;
+
+  while (*head == NULL) {
+    pthread_cond_wait(&notEmpty, &lock);
+  }
+
+  queueNode *temp = *head;
+  queueNode *prio = NULL;
+
+  while (temp != NULL) {
+    if (temp->msgKind == AQ_ALARM) {
+      prio = temp;
+      break;
+    }
+    temp = temp->next;
+  }
+
+  if (*head == NULL) {
+    pthread_mutex_unlock(&lock);
+    return AQ_NO_MSG;
+  }
+
+  while (temp != NULL) {
+    if (temp->msgKind == AQ_ALARM) {
+      pthread_mutex_unlock(&lock);
+      return deleteNode(head, *(int*)temp->msg, msg);;
+    }
+    temp = temp->next;
+  }
+
+  temp = *head;
+  pthread_mutex_unlock(&lock);
+  return deleteNode(head, *(int*)temp->msg, msg);
 }
 
 int aq_size( AlarmQueue aq) {
